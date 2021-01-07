d5bf2c5c5fa8512b59fbe350ce055a22bccccd72
Benjamin Lee <benjamindlee@me.com>
2021-01-07 07:18:01 -0500

    Add BLAST filtering for circular RNAs

---
+++ workflow/scripts/no_blast_hits.py
@@ -0,0 +1,13 @@
+import skbio
+import pandas as pd
+
+df = skbio.io.read(
+    snakemake.input[1],
+    format="blast+7",
+    into=pd.DataFrame,
+)
+
+with open(snakemake.output[0], "w+") as f:
+    for seq in skbio.read(snakemake.input[0], "fasta"):
+        if not len(df.loc[(df.qseqid == seq.metadata["id"]) & (df.evalue < 0.01)]):
+            seq.write(f)
